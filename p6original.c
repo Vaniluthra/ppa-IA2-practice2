729d41957c52061d8c6885a47c92cb6a03b0cb5e
vaniluthra <94587714+Vaniluthra@users.noreply.github.com>
2022-02-12 12:52:22 +0000

    g

--- p6original.c
+++ p6original.c
@@ -2,24 +2,35 @@
 
 void input_string(char *a)
 {
-  printf("Enter Value: ");
-  scanf("%s",a);
+  printf("Enter the string: ");
+  scanf("%s", a);
 }
 
-int str_reverse(char a)
+void str_reverse(char *str, char *rev_str)
 {
+  int n;
+  for (n = 0; str[n]; n++);
+  n--;
+  int i;
+  for (i = 0; str[i]; i++)
+  {
+    rev_str[i] = str[n];
+    n--;
+  }
+  rev_str[i] = '\0';
 }
 
-void output(int a, int str)
+void output(char *a, char *reverse_a)
 {
-  printf("Reversed string of %d is: %d\n",a,str);
+  printf("The reverse of %s is %s\n", a, reverse_a);
 }
 
 int main()
 {
-  int x,z;
-  input_string(&x)
-  z=str(x);
-  output(x,z);
+  char str[10];
+  input_string(str);
+  char rev_str[10];
+  str_reverse(str, rev_str);
+  output(str, rev_str);
   return 0;
 }
